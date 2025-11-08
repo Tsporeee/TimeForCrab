deba38461960f7ab01d68b933d68335d98ba750b
Tian <tsporeee@gmail.com>
2025-11-08 01:51:06 -0800

    Fixed the shake

--- BigCrabGame/Assets/Scripts/ClawScript.cs
+++ BigCrabGame/Assets/Scripts/ClawScript.cs
@@ -11,6 +11,7 @@ public class ClawScript : MonoBehaviour
     public float horizontalMaxPosition = 2f;
     public float verticalMaxPosition = 2f;
     public float speed = 1f;
+    public Vector3 currentPosition;
 
     // Shaking
     public float shakeDuration = 5f;
@@ -19,10 +20,9 @@ public class ClawScript : MonoBehaviour
     // Start is called before the first frame update
     public void Start()
     {
+        
+        // Fake infinite loop
         InvokeRepeating(nameof(shakeClawMethod), 0f, 5f);
-    }
-    public void Update()
-    {
     }
 
     public void FixedUpdate()
@@ -32,34 +32,62 @@ public class ClawScript : MonoBehaviour
 
     public void moveClaw()
     {
+        
+        // Vertical and horizontal, but in game: moving x and z
         float vert = Input.GetAxis("Vertical");
         float horz = Input.GetAxis("Horizontal");
-
         clawTransform.localPosition += (new Vector3(horz, 0f, vert) * speed * Time.deltaTime);
 
+        currentPosition = clawTransform.localPosition;
+        
+        // Container for movement
         clawTransform.localPosition = new Vector3(
             Mathf.Clamp(clawTransform.localPosition.x, -horizontalMaxPosition, horizontalMaxPosition),
             clawTransform.localPosition.y,
             Mathf.Clamp(clawTransform.localPosition.z, -verticalMaxPosition, verticalMaxPosition));
 
     }
+    
     public void shakeClawMethod()
     {
+        
+        // Move this to a normal method to invoke repeating
         StartCoroutine(shakeClaw());
     }
 
-
     IEnumerator shakeClaw()
     {
-        Vector3 startPosition = transform.position;
+        
         float elapsedTime = 0f;
+        float shakeSpeed = 3f;
 
         while (elapsedTime < shakeDuration)
         {
             elapsedTime += Time.deltaTime;
-            float shakeStrength = curve.Evaluate(shakeDuration);
-            clawTransform.localPosition += (new Vector3(Random.Range(-1f, 1f) * shakeStrength, 0f, (Random.Range(-1f, 1f)) * shakeStrength));
+            
+            // -0.5f centers noise, makes half as strong, multiply it by two
+            // Makes a bit of a drift but better than the super jittery other options for now
+            // If we have time: FIX IT
+            float xNoise = ((Mathf.PerlinNoise(Time.time * shakeSpeed, 0f) - 0.5f) * 2);
+            float zNoise = ((Mathf.PerlinNoise(0f, Time.time * shakeSpeed) - 0.5f) * 2);
+            
+            float shakeStrength = curve.Evaluate(elapsedTime/shakeDuration);
+
+            clawTransform.localPosition = currentPosition + new Vector3(xNoise, 0f, zNoise) * shakeStrength;
+
+            // Container for movement
+            clawTransform.localPosition = new Vector3(
+            Mathf.Clamp(clawTransform.localPosition.x, -horizontalMaxPosition, horizontalMaxPosition),
+            clawTransform.localPosition.y,
+            Mathf.Clamp(clawTransform.localPosition.z, -verticalMaxPosition, verticalMaxPosition));
+
             yield return null;
         }
     }
 }
+
+// Graveyard... 
+// Vector3 startPosition = clawTransform.localPosition;
+// clawTransform.localPosition = startPosition + new Vector3(xNoise, 0f, zNoise) * shakeStrength;
+// float xNoise = ((Mathf.PerlinNoise(Time.time * shakeSpeed, 0f) - 0.5f) * 2);
+// float zNoise = ((Mathf.PerlinNoise(0f, Time.time * shakeSpeed) - 0.5f) * 2);
