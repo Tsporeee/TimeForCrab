69ccd7afc3d844883b72063fee9fb1a3ba392da6
Tian <tsporeee@gmail.com>
2025-11-08 22:32:58 -0800

    nightmares about shakey claw

--- BigCrabGame/Assets/Scripts/ClawScript.cs
+++ BigCrabGame/Assets/Scripts/ClawScript.cs
@@ -9,8 +9,9 @@ using UnityEngine.EventSystems;
 
 public class ClawScript : MonoBehaviour
 {
-    public Transform clawTransform = null;
     public Transform clawGrabPointTransform;
+    public float horizontalMaxPosition = 2f;
+    public float verticalMaxPosition = 2f;
     public float speed = 5f;
 
     // Passing other scripts
@@ -18,10 +19,12 @@ public class ClawScript : MonoBehaviour
     public Animator animator;
 
     private bool isDipping;
+    private bool disableMovement;
 
+    private float startPositionY;
 
     // Shaking
-    public float shakeDuration = 5f;
+    public float shakeDuration = 2f;
     public AnimationCurve curve;
 
     private Coroutine clawShakeCoroutine = null;
@@ -30,6 +33,7 @@ public class ClawScript : MonoBehaviour
     public void Start()
     {
         
+        startPositionY = transform.position.y;
         // Fake infinite loop
         InvokeRepeating(nameof(shakeClawMethod), 0f, 2f);
     }
@@ -52,7 +56,10 @@ public class ClawScript : MonoBehaviour
 
     public void FixedUpdate()
     {
-        moveClaw();
+        if (!disableMovement)
+        {
+            moveClaw();
+        }
     }
 
     //private void OnTriggerEnter(Collider other)
@@ -73,31 +80,24 @@ public class ClawScript : MonoBehaviour
 
         transform.position += new Vector3(horz, 0f, vert) * speed * Time.deltaTime;
 
-        //Container for movement
-        // transform.position = new Vector3(
-        // Mathf.Clamp(transform.position.x, -horizontalMaxPosition, horizontalMaxPosition),
-        // transform.position.y,
-        // Mathf.Clamp(transform.position.z, -verticalMaxPosition, verticalMaxPosition));
-
     }
     
     public void shakeClawMethod()
     {
-        
         // Move this to a normal method to invoke repeating
         // Dont restart if theres already a shake running
         if (clawShakeCoroutine == null)
         clawShakeCoroutine = StartCoroutine(shakeClaw());
     }
 
-    // public void StopShakeClaw()
-    // {
-    //     if (clawShakeCoroutine != null)
-    //     {
-    //         StopCoroutine(clawShakeCoroutine);
-    //         clawShakeCoroutine = null;
-    //     }
-    // }
+    public void StopShakeClaw()
+    {
+        if (clawShakeCoroutine != null)
+        {
+            StopCoroutine(clawShakeCoroutine);
+            clawShakeCoroutine = null;
+        }
+    }
 
     IEnumerator shakeClaw()
     {
@@ -111,19 +111,20 @@ public class ClawScript : MonoBehaviour
             
             // -0.5f centers noise
             // Perlin noise makes a bit of a drift but better than the super jittery other options for now
-            float xNoise = (Mathf.PerlinNoise(Time.time * shakeSpeed, 0f) - 0.5f);
-            float zNoise = (Mathf.PerlinNoise(0f, Time.time * shakeSpeed) - 0.5f);
+            float xNoise = Mathf.PerlinNoise(Time.time * shakeSpeed, 0f) - 0.5f;
+            float zNoise = Mathf.PerlinNoise(0f, Time.time * shakeSpeed) - 0.5f;
             
             float shakeStrength = curve.Evaluate(elapsedTime/shakeDuration);
 
             Vector3 newPosition = transform.position + (new Vector3(xNoise, 0f, zNoise) * shakeStrength);
             transform.position = newPosition;
 
-            //Container for movement
-            // transform.position = new Vector3(
-            // Mathf.Clamp(transform.position.x, -horizontalMaxPosition, horizontalMaxPosition),
-            // transform.position.y,
-            // Mathf.Clamp(transform.position.z, -verticalMaxPosition, verticalMaxPosition));
+
+            // Container for movement
+            //clawTransform.position = new Vector3(
+            //Mathf.Clamp(clawTransform.position.x, -horizontalMaxPosition, horizontalMaxPosition),
+            //clawTransform.position.y,
+            //Mathf.Clamp(clawTransform.position.z, -verticalMaxPosition, verticalMaxPosition));
 
             yield return null;
         }
@@ -138,42 +139,43 @@ public class ClawScript : MonoBehaviour
 
         float elapsedTime = 0f;
         float dipDownDuration = 1f;
-        float dipUpDuration = 1f;
+        float dipUpDuration = 0.5f;
 
-        // float startPositionY = transform.position.y;
-        // float endPositionY = transform.position.y - 0.5f;
+        //float startPositionY = transform.position.y;
 
         isDipping = true;
-
+        disableMovement = true;
+        
         while (elapsedTime < dipDownDuration)
         {
+            StopShakeClaw();
             elapsedTime += Time.deltaTime;
 
             // could do this later
             // float dipStrength = curve.Evaluate(elapsedTime / shakeDuration);
-            float lerpSpeed = 2f;
+            float lerpSpeed = 5f;
 
             // Base the down position on your position already pls
-            Vector3 downPosition = new Vector3(transform.position.x, -1f, transform.position.z);
+            Vector3 downPosition = new Vector3(transform.position.x, startPositionY - 2f, transform.position.z);
             Vector3 lerpedDownPosition = Vector3.Lerp(transform.position, downPosition, Time.deltaTime * lerpSpeed);
             transform.position = lerpedDownPosition;
 
             yield return null;
         }
 
-        // transform.position = new Vector3(transform.position.x, endPositionY, transform.position.z);
         yield return new WaitForSeconds(0.5f);
         elapsedTime = 0f;
 
         while (elapsedTime < dipUpDuration)
         {
+            StopShakeClaw();
             elapsedTime += Time.deltaTime;
 
             // could do this later
             // float dipStrength = curve.Evaluate(elapsedTime / shakeDuration);
-            float lerpSpeed = 4f;
+            float lerpSpeed = 15f;
 
-            Vector3 upPosition = new Vector3(transform.position.x, transform.position.y +1f, transform.position.z);
+            Vector3 upPosition = new Vector3(transform.position.x, startPositionY, transform.position.z);
             Vector3 lerpedUpPosition = Vector3.Lerp(transform.position, upPosition, Time.deltaTime * lerpSpeed);
             transform.position = lerpedUpPosition;
 
@@ -181,8 +183,10 @@ public class ClawScript : MonoBehaviour
         }
 
         // Really just in case something goes wrong
-        // transform.position = new Vector3(transform.position.x, startPositionY, transform.position.z);
+        transform.position = new Vector3(transform.position.x, startPositionY, transform.position.z);
         isDipping = false;
+        disableMovement = false;
+        shakeClawMethod();
 
     }
 }
